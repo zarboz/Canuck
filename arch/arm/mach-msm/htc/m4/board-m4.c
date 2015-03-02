6a5d57eae2143d25f3509666f9092a000c7fda53
Paul Keith <javelinanddart@gmail.com>
2015-03-02 14:20:28 -0600

    MSM_THERMAL Board File Hooks for M4
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- arch/arm/mach-msm/htc/m4/board-m4.c
+++ arch/arm/mach-msm/htc/m4/board-m4.c
@@ -2897,11 +2897,21 @@ static struct platform_device msm_tsens_device = {
 };
 
 static struct msm_thermal_data msm_thermal_pdata = {
-	.sensor_id = 9,
-	.poll_ms = 1000,
-	.limit_temp_degC = 60,
-	.temp_hysteresis_degC = 10,
-	.freq_step = 2,
+        .sensor_id = 9,
+        .poll_ms = 150,
+        .shutdown_temp = 75,
+
+        .allowed_max_high = 74,
+        .allowed_max_low = 70,
+        .allowed_max_freq = 384000,
+
+        .allowed_mid_high = 69,
+        .allowed_mid_low = 61,
+        .allowed_mid_freq = 810000,
+
+        .allowed_low_high = 60,
+        .allowed_low_low = 55,
+        .allowed_low_freq = 1350000,
 };
 
 /* 8930 Phase 2 */
